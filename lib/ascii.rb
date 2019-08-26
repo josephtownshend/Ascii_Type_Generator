5cf15c29d95ec63875b1f20a335e87ae0da105fe
joseph townshend <joetownshend@mac.com>
2019-08-26 13:33:16 +0100

    test: failing ascii A

--- lib/ascii.rb
+++ lib/ascii.rb
@@ -1,5 +1,21 @@
 class Ascii_generator
+
   def letter_matcher(user_input)
-    return "A"
+    if user_input == "A"
+      print [
+        "//////////",
+        "//////////",
+        "////  ////",
+        "////  ////",
+        "//////////",
+        "//////////",
+        "////  ////",
+        "////  ////",
+        "////  ////"
+      ].join("\n")
+    else
+      return nil
+    end
   end
+
 end
--- spec/ascii_spec.rb
+++ spec/ascii_spec.rb
@@ -3,6 +3,16 @@ require 'ascii'
 describe Ascii_generator do
   it "should return ASCII A when user inputs A" do
     ascii_gen = Ascii_generator.new
-    expect(ascii_gen.letter_matcher("A")).to eq("A")
+    expect(ascii_gen.letter_matcher("A")).to output(
+      ["//////////",
+      "//////////",
+      "////  ////",
+      "////  ////",
+      "//////////",
+      "//////////",
+      "////  ////",
+      "////  ////",
+      "////  ////"
+    ].join("\n")).to_stdout
   end
 end
