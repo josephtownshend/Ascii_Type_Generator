8ef0004377f143f1a26541590fc9c708cfe4aa19
joseph townshend <joetownshend@mac.com>
2019-08-26 13:56:52 +0100

    feat: can return an ASCII A when user inputs A - with passing test

--- spec/ascii_spec.rb
+++ spec/ascii_spec.rb
@@ -3,7 +3,7 @@ require 'ascii'
 describe Ascii_generator do
   it "should return ASCII A when user inputs A" do
     ascii_gen = Ascii_generator.new
-    expect(ascii_gen.letter_matcher("A")).to output(
+    expect { ascii_gen.letter_matcher("A") }.to output(
       ["//////////",
       "//////////",
       "////  ////",
