4c7065c06ebb8e7a5092e33e5ea65296ed186c9d
joseph townshend <joetownshend@mac.com>
2019-08-27 09:16:26 +0100

    feat: array for ascii A working with test

--- lib/ascii.rb
+++ lib/ascii.rb
@@ -1,19 +1,16 @@
 class Ascii_generator
 
+  attr_reader :ascii_A
+
+  def initialize
+    @ascii_A = ["//////////", "//////////", "////  ////", "////  ////", "//////////", "//////////", "////  ////", "////  ////",  "////  ////", "////  ////"]
+  end
+
   def letter_matcher(user_input)
     if user_input == "A"
-      print [
-        "//////////",
-        "//////////",
-        "////  ////",
-        "////  ////",
-        "//////////",
-        "//////////",
-        "////  ////",
-        "////  ////",
-        "////  ////",
-        "////  ////"
-      ].join("\n")
+    print ascii_A.join("\n")
+
+
     elsif user_input == "B"
       print [
         "//////////",
