26b7eb629fa3df06c2c3311e1bf6b856380edd5f
joseph townshend <joetownshend@mac.com>
2019-08-29 12:52:26 +0100

    feat: slices now work with B

--- lib/ascii.rb
+++ lib/ascii.rb
@@ -8,14 +8,14 @@ class Ascii_generator
     @ascii_C = ["//////////", "//////////", "////      ", "////      ", "////      ", "////      ", "////      ", "////      ", "//////////", "//////////"]
     @ascii_slices = ["//////////\n", "////      \n", "////  ////\n","//////////", "////  ////", "////      "]
     @letter_A = [0,0,2,2,0,0,2,2,2,4]
+    @letter_B = [0,0,2,2,0,0,2,2,0,3]
   end
 
   def letter_matcher(user_input)
     if user_input == "A"
       @letter_A.each {|x| print @ascii_slices[x]}
-
     elsif user_input == "B"
-      print ascii_B.join("\n")
+      @letter_B.each {|x| print @ascii_slices[x]}
     elsif user_input == "C"
       print ascii_C.join("\n")
     else
